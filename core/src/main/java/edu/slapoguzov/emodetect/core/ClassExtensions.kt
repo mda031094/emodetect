586ba44ba9ed24e055c2d97473905b7247ed9eea
mda031094 <76900108+mda031094@users.noreply.github.com>
2021-01-08 14:42:29 +0300

    Update ClassExtensions.kt
    test

--- core/src/main/java/edu/slapoguzov/emodetect/core/ClassExtensions.kt
+++ core/src/main/java/edu/slapoguzov/emodetect/core/ClassExtensions.kt
@@ -7,3 +7,21 @@ fun <T> Class<T>.readResource(resourceName: String): String {
 fun <T> Class<T>.readResourceByLines(resourceName: String): List<String> {
     return this.classLoader.getResourceAsStream(resourceName).reader().readLines()
 }
+ private tailrec fun _process2(emotions: MutableSet<EmotionType>): MutableSet<EmotionType> {
+        val result = emotions.toMutableSet()
+        val rule = rules.find { result.containsAll(it.first) }
+                ?: return result
+        logger.info { "apply rule: ${rule.first} to ${rule.second}" }
+        result.removeAll(rule.first)
+        result.add(rule.second)
+        return _process(result)
+    }
+ private tailrec fun _process2(emotions: MutableSet<EmotionType>): MutableSet<EmotionType> {
+        val result = emotions.toMutableSet()
+        val rule = rules.find { result.containsAll(it.first) }
+                ?: return result
+        logger.info { "apply rule: ${rule.first} to ${rule.second}" }
+        result.removeAll(rule.first)
+        result.add(rule.second)
+        return _process(result)
+    }
